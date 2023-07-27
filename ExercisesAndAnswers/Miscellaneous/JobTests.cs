adedbd8bbe65e55c0898a961b5b7995ed573d157
Frost <frostsword@vp.pl>
2023-07-27 12:19:15 +0200

    job application tests

---
+++ ExercisesAndAnswers/Miscellaneous/JobTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesAndAnswers.Miscellaneous
+{
+    internal class JobTests
+    {
+        static int GetLongest(List<bool> values)
+        {
+            int currentLength = 0;
+            int sequenceLength = 0;
+            foreach (bool item in values)
+            {
+                if (item) currentLength++;
+                else currentLength = 0;
+                if (currentLength > sequenceLength) sequenceLength = currentLength;
+            }
+            return sequenceLength;
+        }
+
+        
+
+        
+
+    }
+}
--- ExercisesAndAnswers/Program.cs
+++ ExercisesAndAnswers/Program.cs
@@ -33,19 +33,37 @@ internal class Program
                 "......\n" +
                 "......\n" +
                 "......";
-      //  Console.WriteLine(MathWorks.PathFinder2(a));
+            //  Console.WriteLine(MathWorks.PathFinder2(a));
 
             //Fibonacci memoization
-           // Func<int, int> fibonacci = null;
-           // fibonacci = Memoizer.Memoize((int n1) => Fibonacci(n1, fibonacci));
+            // Func<int, int> fibonacci = null;
+            // fibonacci = Memoizer.Memoize((int n1) => Fibonacci(n1, fibonacci));
             //Console.WriteLine($"{fibonacci(10)}");
 
             //int max value = 2147483647
-         //   Console.WriteLine($"{ExercisesAndAnswers._4Kyu.Kata.Factorial2(100)}");
+            //   Console.WriteLine($"{ExercisesAndAnswers._4Kyu.Kata.Factorial2(100)}");
 
-         //   Console.WriteLine($"{ExercisesAndAnswers._6Kyu.Kata.High("what time are we climbing up to the volcano")}");
+            //   Console.WriteLine($"{ExercisesAndAnswers._6Kyu.Kata.High("what time are we climbing up to the volcano")}");
 
-           
+            #region job test
+            /*
+            List<IProject> currentProjects = new List<IProject>();
+            currentProjects.Add(new project {Name="a", Start=new DateTime(2023,07,27), End=new DateTime(2023,07,29)});
+            currentProjects.Add(new project { Name = "b", Start = new DateTime(2023, 07, 16), End = new DateTime(2023, 07, 20) });
+            project toCheck = new project() { Name = "c", Start = new DateTime(2023, 07, 01), End = new DateTime(2023, 07, 28) };
+
+            static bool IsProjectPossible(List<IProject> existing, IProject project)
+            {
+                foreach (IProject item in existing)
+                {
+                    if (item.Start.Date < project.End.Date && project.Start.Date < item.End.Date) return false;
+                }
+                return true;
+            }
+
+            Console.WriteLine(IsProjectPossible(currentProjects, toCheck));
+            */
+            #endregion
 
         }
 
@@ -60,7 +78,27 @@ internal class Program
             return fibonacci(n1 - 1) + fibonacci(n1 - 2);
         }
 
+        #region job test
+        /*
+        public class project : IProject
+        {
+            public string Name {get; set;}
+            public DateTime Start { get; set; }
+            public DateTime End { get; set; }
+        }
+        public interface IProject
+        {
+            string Name { get; set; }
+            DateTime Start { get; set; }
+            DateTime End { get; set; }
+        }*/
+        #endregion
+
+
+
+
 
 
     }
+
 }
